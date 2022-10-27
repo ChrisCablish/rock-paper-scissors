d0e3d153b618f4db7c8987399989d001802d8ce4
Chris <chriscablish@hotmail.com>
2022-10-27 13:14:03 -0500

    Refine gamePlay() to ensure functionality & brevity

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Define variables and invoke gamePlay() inside of console.log
-let userChoice = prompt('Rock, Paper, or Scissors?');
-userChoice = userChoice.toLowerCase();
-let computerChoice = computerNumber();
-console.log(gamePlay(computerChoice,userChoice));
-
 // Create function to make computer selection 
 function computerNumber() {
     const number = (Math.random() * 3)
@@ -14,39 +8,61 @@ function computerNumber() {
 }   else return 'scissors'
 }
 
-// Create function to run one round of the game
-function gamePlay (computerChoice,userChoice) {
-    if (computerChoice===userChoice) {
-        console.log("It's a tie!");
-    } else if (userChoice=='rock'&&computerChoice=='scissors'||userChoice=='paper'&&computerChoice=='rock'|| userChoice=='scissors'&&computerChoice=='paper') {
-        console.log('User chose ' + userChoice + '. Computer chose ' + computerChoice + '. User Wins!')
-    }  else if (userChoice != 'rock' || 'paper' || 'scissors') {
-        console.log('Please check your spelling');
-    }  else {
-        console.log ('User chose ' + userChoice + '. Computer chose ' + computerChoice + '. Computer Wins!');
+let playerScore = 0;
+let computerScore = 0;
+
+// Create function to play one round of RPS
+/*function gamePlay (computerChoice,userChoice) {
+
+
+    if (computerChoice==userChoice) {
+    console.log("It's a tie!");
+    } 
+    
+    else if (userChoice=='rock'&&computerChoice=='scissors'||userChoice=='paper'&&computerChoice=='rock'|| userChoice=='scissors'&&computerChoice=='paper') {
+        console.log('User chose ' + userChoice + '. Computer chose ' + computerChoice + `. User wins this round! Score: ${playerScore} to ${computerScore}.`)
+    }  
+    
+    
+    else if (userChoice != 'rock' || 'paper' || 'scissors') {
+    console.log('Please check your spelling');
+    }  
+    
+    else {
+    console.log ('User chose ' + userChoice + '. Computer chose ' + computerChoice + `. Computer wins this round! Score: ${playerScore} to ${computerScore}.`);
     }
+}*/
+
+function gamePlay (computerChoice,userChoice) {
+
+if (userChoice=='rock'&&computerChoice=='scissors'||userChoice=='paper'&&computerChoice=='rock'|| userChoice=='scissors'&&computerChoice=='paper') {
+    console.log('User chose ' + userChoice + '. Computer chose ' + computerChoice + `. User wins!.`)
+
+} else if (userChoice=='scissors'&&computerChoice=='rock'||userChoice=='rock'&&computerChoice=='paper'|| userChoice=='paper'&&computerChoice=='scissors') {
+    console.log('User chose ' + userChoice + '. Computer chose ' + computerChoice + `. Computer wins!.`)
+
+} else if (computerChoice==userChoice) {
+        console.log("It's a tie!");
+    
+} else console.log('Please check your spelling.');
+
+
+
 }
     
+//Create a function so that the game has five rounds
+function gameRounds() {
+    for (let i = 0; i < 5; i++) {
+        // Define variables and invoke gamePlay() inside of console.log after each round
+        let userChoice = prompt('Rock, Paper, or Scissors?');
+        userChoice = userChoice.toLowerCase();
+        let computerChoice = computerNumber();
+        console.log(gamePlay(computerChoice,userChoice));
+    }   
 
+}
 
-
-
-
-    
-    /*if (computerChoice == 'rock' && userChoice == 'paper') {
-        return 'Computer chose ' + computerChoice + '. User chose ' + userChoice + '. Paper covers rock. User Wins.'
-    } else if (computerChoice == 'paper' && userChoice == 'rock'){
-        return 'Computer chose ' + computerChoice + '. User chose ' + userChoice + '. Paper covers rock. Computer Wins.'
-    } else if (computerChoice == 'paper' && userChoice == 'paper'){
-        return 'Computer chose ' + computerChoice + '. User chose ' + userChoice + '. The game is a tie.'
-    } else if (computerChoice == 'rock' && userChoice == 'rock'){
-        return 'Computer chose ' + computerChoice + '. User chose ' + userChoice + '. The game is a tie.'
-    } else return 'Please type carefully. Only rock and paper are valid prompt responses.'
-
-
-    
-} 
-*/  
+gameRounds();
 
 
 

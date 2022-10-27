f66adb75c41809bd8e44a23076c697e87268f895
Chris <chriscablish@hotmail.com>
2022-10-27 13:49:29 -0500

    Create loop function so that the game has five rounds

--- script.js
+++ script.js
@@ -11,46 +11,31 @@ function computerNumber() {
 let playerScore = 0;
 let computerScore = 0;
 
+
 // Create function to play one round of RPS
-/*function gamePlay (computerChoice,userChoice) {
-
-
-    if (computerChoice==userChoice) {
-    console.log("It's a tie!");
-    } 
-    
-    else if (userChoice=='rock'&&computerChoice=='scissors'||userChoice=='paper'&&computerChoice=='rock'|| userChoice=='scissors'&&computerChoice=='paper') {
-        console.log('User chose ' + userChoice + '. Computer chose ' + computerChoice + `. User wins this round! Score: ${playerScore} to ${computerScore}.`)
-    }  
-    
-    
-    else if (userChoice != 'rock' || 'paper' || 'scissors') {
-    console.log('Please check your spelling');
-    }  
-    
-    else {
-    console.log ('User chose ' + userChoice + '. Computer chose ' + computerChoice + `. Computer wins this round! Score: ${playerScore} to ${computerScore}.`);
-    }
-}*/
 
 function gamePlay (computerChoice,userChoice) {
 
 if (userChoice=='rock'&&computerChoice=='scissors'||userChoice=='paper'&&computerChoice=='rock'|| userChoice=='scissors'&&computerChoice=='paper') {
-    console.log('User chose ' + userChoice + '. Computer chose ' + computerChoice + `. User wins!.`)
+    playerScore++;
+    console.log('User chose ' + userChoice + '. Computer chose ' + computerChoice + `. User wins this round! The score is User: ${playerScore} Computer: ${computerScore}.`)
 
 } else if (userChoice=='scissors'&&computerChoice=='rock'||userChoice=='rock'&&computerChoice=='paper'|| userChoice=='paper'&&computerChoice=='scissors') {
-    console.log('User chose ' + userChoice + '. Computer chose ' + computerChoice + `. Computer wins!.`)
+    computerScore++;
+    console.log('User chose ' + userChoice + '. Computer chose ' + computerChoice + `. Computer wins this round! The score is User: ${playerScore} Computer: ${computerScore}.`)
 
 } else if (computerChoice==userChoice) {
-        console.log("It's a tie!");
+        console.log("It's a tie! Try that round again.");
+         
+
     
 } else console.log('Please check your spelling.');
-
-
-
 }
     
-//Create a function so that the game has five rounds
+
+
+//Create loop function so that the game has five rounds
+
 function gameRounds() {
     for (let i = 0; i < 5; i++) {
         // Define variables and invoke gamePlay() inside of console.log after each round
@@ -59,6 +44,13 @@ function gameRounds() {
         let computerChoice = computerNumber();
         console.log(gamePlay(computerChoice,userChoice));
     }   
+
+    if (playerScore > computerScore) {
+        console.log(`You're the winner! Final Score: ${playerScore} to ${computerScore}`);
+    } else if (computerScore > playerScore) {
+        console.log(`You're the loser. Final Score: ${playerScore} to ${computerScore}`);
+    } else
+        console.log(`You tied. Final Score: ${playerScore} to ${computerScore}`);
 
 }
 
